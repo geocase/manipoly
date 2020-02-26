8f46b1235532780a50762825056550031a8ac42d
Casey Georgeson <caseys.georgeson@gmail.com>
2020-02-25 21:50:19 -0700

    Random "shapes"

--- src/main.cc
+++ src/main.cc
@@ -3,40 +3,46 @@
 #include "sector.h"
 #include "window.h"
 
+#define NODES 10
+
 int main() {
+	srand(time(NULL));
 	cWindow primWin;
 
-	sNode *points = new sNode[4];
-	points[0].x = 10;
-	points[0].y = 10;
-	points[1].x = 20;
-	points[1].y = 10;
-	points[2].x = 20;
-	points[2].y = 20;
-	points[3].x = 10;
-	points[3].y = 20;
-
-	
+	sNode *points = new sNode[NODES];
+		
 	cSector quad;
 
-	for(int i = 0; i < 4; i++) {
-		if(i + 1 > 3) {
-			quad.AddFace(new cLineDef(points[i], points[i-1]));
+	for(int i = 0; i < NODES; i++) {
+		if(i + 1 > NODES - 1) {
+			quad.AddFace(new cLineDef(&points[i], &points[0]));
 		} else {
-			quad.AddFace(new cLineDef(points[i], points[i+1]));
+			quad.AddFace(new cLineDef(&points[i], &points[i+1]));
 		}
 	}
 
 	sNode tempA;
 	sNode tempB;
 
-	for(int i = 0; i < 4; i++) {
-		tempA = quad.faces.at(i).a;
-		tempB = quad.faces.at(i).b;
-
-		std::cout << tempA.x << ", " << tempA.y << " : " << tempB.x << ", " << tempB.y << std::endl;
+	sColor_t col;
+	col.r = 255;
+	col.g = 255;
+	col.b = 255;
+	col.a = 255;
+	for(int m = 0; m < NODES; m++) {
+			points[m].x = rand() % primWin.winx;
+			points[m].y = rand() % primWin.winy;
 	}
 
+	for(int k = 0; k < 10000; k++) {	
+		for(int i = 0; i < quad.faceCount; i++) {
+			tempA = *(quad.faces.at(i).a);
+			tempB = *(quad.faces.at(i).b);
+			primWin.DrawLine(tempA.x, tempA.y, tempB.x, tempB.y, col);
+		}
+		
+		primWin.Update();
+	}
 	std::cout << "faces: " << quad.faceCount << std::endl;
 
 	return 0;
