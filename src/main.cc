554d71d60572075b20e0e7f6e0458dafdc0e97b1
Casey Georgeson <caseys.georgeson@gmail.com>
2020-02-28 23:24:02 -0700

    Update demo

--- src/main.cc
+++ src/main.cc
@@ -4,28 +4,34 @@
 #include "window.h"
 #include "input.h"
 
-#define NODES 10
+#define NODES 6
 
 int main() {
 	srand(time(NULL));
 	cWindow primWin;
 	cInput primIn;
 	cVertex *points = new cVertex[NODES];
+	points[0].PlaceAt(primWin.winx / 2, primWin.winy / 2);
+	points[1].PlaceAt(primWin.winx / 2 + 100, primWin.winy / 2);
+	points[2].PlaceAt(primWin.winx / 2, primWin.winy / 2 - 50);
+	points[3].PlaceAt(0, 0);
+	points[4].PlaceAt(primWin.winx / 2 + 100, primWin.winy / 2 - 5);
+	points[5].PlaceAt(0, 0);
+
 	/*
-	points[0].PlaceAt(primWin.winx / 2 - 100, primWin.winy / 2 - 100);
-	points[1].PlaceAt(primWin.winx / 2 + 100, primWin.winy / 2 - 100);
-	points[2].PlaceAt(primWin.winx / 2 + 100, primWin.winy / 2 + 100);
-	points[3].PlaceAt(primWin.winx / 2 - 100, primWin.winy / 2 + 100);
-	*/
 	for(int l = 0; l < NODES; l++) {
 		points[l].PlaceAt(rand() % primWin.winx, rand() % primWin.winy);
 	}
-
+	*/
 	cPolygon quad;
 //	quad.BuildFromVertexArray(points, NODES);	
 
 	cPath p;
 	p.BuildFromVertexArray(points, NODES);	
+
+	cSegment *segA = new cSegment(&points[0], &points[1]);
+	cSegment *segB = new cSegment(&points[2], &points[3]);
+	cSegment *segC = new cSegment(&points[4], &points[5]);
 
 	cVertex tempA;
 	cVertex tempB;
@@ -36,12 +42,24 @@ int main() {
 	col.b = 255;
 	col.a = 255;
 
-	for(int k = 0; k < 15000; k++) {				
+	for(int k = 0; k < 15000; k++) {
+		
+		primWin.DrawLine(segA->a->x, segA->a->y, segA->b->x, segA->b->y, col);
+		primWin.DrawLine(segB->a->x, segB->a->y, segB->b->x, segB->b->y, col);
+		primWin.DrawLine(segC->a->x, segC->a->y, segC->b->x, segC->b->y, col);
+
+		points[3] = segA->GetLocationOfSubdividedVertex(.1);
+
+		points[5] = segB->GetLocationOfSubdividedVertex(.25);
+
+		points[1].Rotate(points[0].x, points[0].y, .001);
+		/*
 		for(int i = 0; i < p.lineDefCount; i++) {
 			tempA = *(p.lineDefs.at(i).a);
 			tempB = *(p.lineDefs.at(i).b);
 			primWin.DrawLine(tempA.x, tempA.y, tempB.x, tempB.y, col);
 		}
+		*/
 		primWin.Update();
 		primIn.Update();
 	}
